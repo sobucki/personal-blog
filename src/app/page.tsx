9dbfa4431fda96b3b5fb031ff1def8f355c04802
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-09-18 07:29:41 -0300

    Remove contact page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { getSortedPostsData, PostData } from "@/lib/posts";
+import Link from "next/link";
+
 export default function Home() {
+  const allPostsData: PostData[] = getSortedPostsData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div>
+      <h1>Meu Blog</h1>
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small>{date}</small>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
